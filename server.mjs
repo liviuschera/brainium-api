7f0616772474521da1ea69fd4fa929441b95c4de
liviuschera <liviu115@gmail.com>
2020-06-21 09:27:21 +1000

    added process.env.PORT

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import handleRank from './controllers/rank.controller.mjs';
 import handleGetUser from './controllers/sign-up.controller.mjs';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT ?? 5000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
