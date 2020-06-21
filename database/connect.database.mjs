fe1a85f84d7a41d47b8dbb8406c5b3de879d2a72
liviuschera <liviu115@gmail.com>
2020-06-21 10:52:42 +1000

    connected postgres in nodejs

--- database/connect.database.mjs
+++ database/connect.database.mjs
@@ -1,12 +1,14 @@
 import knex from 'knex';
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'test',
-    database: 'brainium',
-  },
+   client: 'pg',
+   connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+      // host: '127.0.0.1',
+      // user: 'postgres',
+      // password: 'test',
+      // database: 'brainium',
+   },
 });
 export default db;
